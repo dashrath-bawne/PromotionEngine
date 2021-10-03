daba1b2b8a5fc58553239e1903de19b129a8b572
dbawne <dashrath.bawne@oracle.com>
2021-10-03 15:12:51 +0530

    Refactored CombinedPromotion to apply promotion or actual value based on availability of two items in cart. Test case to apply combined promotion for C and D - SUCCESS

--- src/main/java/org/example/promotionengine/impl/CombinedPromotion.java
+++ src/main/java/org/example/promotionengine/impl/CombinedPromotion.java
@@ -24,7 +24,31 @@ public class CombinedPromotion implements Promotion {
     @Override public int apply(Map<Character, Integer> cartItems) {
 
         // Number of items in cart which have same firstItemId as mentioned in this promotion
-        int firstItemUnits = cartItems.get(firstItemId);
-        return (firstItemUnits * ItemCache.getItemCache().get(firstItemId));
+        int firstItemUnits = cartItems.getOrDefault(firstItemId, 0);
+        // Number of items in cart which have same secondItemId as mentioned in this promotion
+        int secondItemUnits = cartItems.getOrDefault(secondItemId, 0);
+
+        // both items have same numbers of unit
+        if (firstItemUnits == secondItemUnits) {
+            return (firstItemUnits * promotionalPrice);
+            // Only first item is available in cart. Apply actual price of first item directly
+        } else if (secondItemUnits == 0) {
+            return firstItemUnits * ItemCache.getItemCache().get(firstItemId);
+            // Only second item is available in cart. Apply actual price of second item directly
+        } else if (firstItemUnits == 0) {
+            return secondItemUnits * ItemCache.getItemCache().get(secondItemId);
+            // Both items are available in cart but there number of units aren't same
+        } else {
+            // Number of units for which actual price should be applied
+            int promotionNotApplicableUnits = Math.abs(firstItemUnits - secondItemUnits);
+            // Number of times promotion value should be applied
+            int promotionApplicableUnits = Math.min(firstItemUnits, secondItemUnits);
+            // Item ID which should be considered to get the actual price
+            Character promotionNotApplicableItemId =
+                    Math.max(firstItemUnits, secondItemUnits) == firstItemUnits ? firstItemId : secondItemId;
+            // sum of promotional price and actual price
+            return ((promotionNotApplicableUnits * ItemCache.getItemCache().get(promotionNotApplicableItemId)) + (
+                    promotionApplicableUnits * promotionalPrice));
+        }
     }
 }
