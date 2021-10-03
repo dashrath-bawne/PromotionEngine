715e40d4d6707460deca7684935edd33f70b551f
dbawne <dashrath.bawne@oracle.com>
2021-10-03 13:40:15 +0530

    Refactored Promotion interface and created first promotion type i.e. IndividualPromotion. It's meant for single item

---
+++ src/main/java/org/example/promotionengine/impl/IndividualPromotion.java
@@ -0,0 +1,35 @@
+package org.example.promotionengine.impl;
+
+import java.util.Map;
+
+import org.example.promotionengine.interfaces.Promotion;
+
+/**
+ * This promotion type is meant for individual item (specific itemId)
+ */
+public class IndividualPromotion implements Promotion {
+
+    // Item ID for which this promotion applies
+    private Character itemId;
+    // Indicates number of units of specific item ID
+    private int promotionalCount;
+    // Indicates promotional price to apply
+    private int promotionalPrice;
+
+    public IndividualPromotion(Character itemId, int promotionalCount, int promotionalPrice) {
+
+        this.itemId = itemId;
+        this.promotionalCount = promotionalCount;
+        this.promotionalPrice = promotionalPrice;
+    }
+
+    @Override public int apply(Map<Character, Integer> cartItems) {
+
+        // Number of items in cart which have same itemId as mentioned in this promotion
+        int numberOfItems = cartItems.get(itemId);
+        // Number of times promotional price should be applied
+        int matchedCount = numberOfItems / promotionalCount;
+
+        return ((matchedCount * promotionalPrice));
+    }
+}
--- src/main/java/org/example/promotionengine/interfaces/Promotion.java
+++ src/main/java/org/example/promotionengine/interfaces/Promotion.java
@@ -1,8 +1,17 @@
 package org.example.promotionengine.interfaces;
 
+import java.util.Map;
+
 /**
  * Super type for all types of promotions. Each promotion type should implement apply method
  */
 public interface Promotion {
 
+    /**
+     * This method looks for specific item type in cartItems. Gets its corresponding unit count and perform logic as mentioned to calculate final order value.
+     *
+     * @param cartItems - holds mapping of cart item and its corresponding unit count
+     * @return int - total order value for specific type of item
+     */
+    int apply(Map<Character, Integer> cartItems);
 }
